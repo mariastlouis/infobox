02452d3222ec04482e8a76ba5e99b04403bc5b05
Patrick Neel <patrick.neel@colorado.edu>
2017-09-03 15:54:12 -0600

    button fuctionality

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 $('#submit-button').on('click', saveIdea);
-
+// $('.idea-article').on('click', deleteIdea);
 
 
 
@@ -11,29 +11,30 @@ function saveIdea (e) {
 	
 	$('.bookmark-list').prepend(
 		`<article class="idea-article">
-		
 		<h2 class="idea-title">${title}</h2> 
-		
-
-			<img src ="images/delete.svg" class="icon-buttons delete-button right" />
-
-		
+		<div class="delete-button-div icon-buttons delete-button right">
+		</div>
 		<p contenteditable="true" class="idea-paragraph">${body}</p>
-		
-		
-		
-		<p class="quality-rank"> 
-			<img src ="images/upvote.svg" class ="icon-buttons upvote-button" />
-		<img src = "images/downvote.svg" class="icon-buttons downvote-button" />
-
-		quality: <span class = "quality-content"> swill</span> </p>
-
+		<div class="quality-rank"> 
+		<div class="upvote-button-div icon-buttons upvote-button">
+		</div>
+		<div class="downvote-button-div icon-buttons downvote-button"> 
+		</div>
+		quality: <span class = "quality-content"> swill</span> </div>
 		<hr /> 
-		
-
-	</article>`
-
-
- 
-		)
+		</article>`)
+	$('.main-title').val("");
+	$('.idea-input').val("");
 }
+
+$('.bookmark-list').on('click', '.delete-button-div', function() {
+	console.log('work')
+	$(this).closest('.idea-article').remove();
+});
+
+// function deleteIdea(e){
+// 	e.preventDefault();
+// 	if    $(this).closest
+// 	console.log("work");
+// }
+
