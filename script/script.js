1ba27a627da5c3a46f9272c5b0ca90435958adef
mariastlouis <msantray@yahoo.com>
2017-09-06 11:36:18 -0600

    tried to add local storage

--- script/script.js
+++ script/script.js
@@ -1,14 +1,34 @@
+var ideaArray = [];
+var title = $('.main-title').val();
+	var body = $('.idea-input').val();
+	var status = 'swill';
+
 $('#submit-button').on('click', saveIdea);
 // $('.idea-article').on('click', deleteIdea);
 
 
+function Idea (title, body ) {
+	this.title = title;
+	this.body = body; 
+	this.status = 'swill'; 
+	this.id = Date.now();
+}
 
+function addIdea () {
+	// var title = $('.main-title').val();
+	// var body = $('.idea-input').val();
+	// var status = 'swill';
+	var anotherIdea = new Idea (title, body, status);
+	saveIdea(anotherIdea);
+	ideaArray.push(antherIdea)
+	storeIdea();
+}
 
 function saveIdea (e) {
 	e.preventDefault();
-	var title = $('.main-title').val();
-	var body = $('.idea-input').val();
-	var status = 'swill';
+	// var title = $('.main-title').val();
+	// var body = $('.idea-input').val();
+	// var status = 'swill';
 	
 	$('.bookmark-list').prepend(
 		`<article class="idea-article">
@@ -31,6 +51,40 @@ function saveIdea (e) {
 $('.bookmark-list').on('click', '.delete-button-div', function() {
 	$(this).closest('.idea-article').remove();
 });
+
+
+function storeIdea () {
+	localStorage.setItem("ideaArray", JSON.stringify(ideaArray));
+}
+
+function getIdea () {
+	var retrieveIdea = JSON.parse(ideaArray);
+	return retrieveIdea;
+}
+
+// create an empty array to fill with objects 
+
+// create an object
+
+// fill the object with info from page
+
+// display the object on the page 
+
+// store the object in local storage
+
+// parse items
+
+// get objects 
+
+// on page load display objects
+
+ 
+
+
+
+
+
+
 
 // $('.bookmark-list').on('click', '.upvote-button-div', function() {
 // 	$('.quality-content').text(($('.quality-content').text() == 'swill') ? 'genius' : 'plausible');
